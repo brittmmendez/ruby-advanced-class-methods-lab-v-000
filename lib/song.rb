d001cd687e3b067329c3cba8ed9aa99f487b455e
Brittany Morris <brittmmendez@gmail.com>
2017-12-12 19:57:06 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,12 +33,11 @@ class Song
     self.all.detect{|song| song.name == name}
   end
 
-  def self.find_or_create_by_name(song)
-    if self.find_by_name(song)
-      self.create_by_name(song)
-    else
-      self.find_by_name(song)
-    end
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+    self.find_by_name(name)
+    else self.create_by_name(name)
+  end
   end
 
 end
